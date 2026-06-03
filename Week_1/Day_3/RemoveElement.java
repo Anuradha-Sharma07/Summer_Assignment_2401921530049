f7743e7c6df031141bf72f4e7c1472f6945e05e4
Anuradha-Sharma07 <anu071424@gmail.com>
2026-06-03 23:44:06 +0530

    Adding Remove Element-27

---
+++ Week_1/Day_3/RemoveElement.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class RemoveElement {
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+        int k = removeElement(nums, val);
+        System.out.println("k = " + k);
+        System.out.print("Array after removal: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
